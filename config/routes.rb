f4a3fa9b2802f7cc402278a47f4d7d836f7f8824
Ytavrech <ytavrech@bestpeers.com>
2022-09-14 03:11:10 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'article#index'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create', as: 'log_in'
+end
